67e2bddae30ae62ec3cae2b564590dc09dec37b2
Max Rem <max@geeksploit.me>
2018-07-06 22:18:46 +0300

    feat: Add a helper for Array to List conversion

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,8 +2,12 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
@@ -15,6 +19,15 @@ public class JsonUtils {
             e.printStackTrace();
             return null;
         }
+    }
+
+    private static List<String> jsonArrayToList(JSONArray jsonArray) throws JSONException {
+        // TODO: is there a better way to split the array?
+        List<String> alsoKnownAs = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            alsoKnownAs.add(jsonArray.getString(i));
+        }
+        return alsoKnownAs;
     }
 }
 
