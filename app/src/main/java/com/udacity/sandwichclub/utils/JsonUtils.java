8cfad7cbe425399e49b800e5a18126cf4bff7105
Max Rem <max@geeksploit.me>
2018-07-06 22:09:50 +0300

    feat: Implement Sandwich JSON parsing stub

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,10 +2,19 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try {
+            JSONObject j = new JSONObject(json);
+            return new Sandwich();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
 
